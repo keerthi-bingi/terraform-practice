f720039778f5f64d78d1dbf9771fd8679404e736
keerthi-bingi <keerthibingi1312@gmail.com>
2026-03-14 01:06:47 +0530

    Add tf files

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -1,7 +1,7 @@
-resource "aws_instance" "example" {
+resource "aws_instance" "ec2_instance" {
   ami           = "ami-0220d79f3f480ecf5"
   instance_type = "t3.micro"
-  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  vpc_security_group_ids = [aws_security_group.ec2_sg_tf.id]
 
   tags = {
     Name = "terraform"
@@ -9,7 +9,7 @@ resource "aws_instance" "example" {
   }
 }
 
-resource "aws_security_group" "allow_tls" {
+resource "aws_security_group" "ec2_sg_tf" {
   name        = "allow-all-terraform" # this is for AWS account
   description = "Allow TLS inbound traffic and all outbound traffic"
 
